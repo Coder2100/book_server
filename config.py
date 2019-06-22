e450ebed9ab805afdedc4f651cf57ad553695e8b
@Coder2100 <mtotsoz@gmail.com>
2019-06-23 00:47:22 +0200

    first commit

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SECRET_KEY = 'th*is-r(030$eallydhdgdkdhhdaDJa**2-needs-tt63o-be-ch\\(anged'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
